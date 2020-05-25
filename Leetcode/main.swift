5f1a49c4d6575b3df3d8ac01875c65dd73baea94
RodinYTY <1418583869@qq.com>
2020-05-25 10:01:40 +0800

    初始化仓库

---
+++ Leetcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Leetcode
+//
+//  Created by RodinYTY on 2020/5/25.
+//  Copyright © 2020 RodinYTY. All rights reserved.
+//
+
+import Foundation
+
+var solution = Solution
+
